d550bdebb9e3d575fc4ab603440873a9f4be1383
bloom-lmh <1357526355@qq.com>
2024-04-15 16:40:42 +0800

    props学习

--- code/src/App.vue
+++ code/src/App.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>hello world vue3</div>
+  <!-- 向子组件传递数据 -->
+  <Person :list="persons" />
 </template>
-<script lang="ts"></script>
+<script lang="ts" setup name="App">
+  import Person from './components/Person.vue'
+  import { reactive } from 'vue'
+  // 导入数据类型
+  import { type Persons } from './types'
+  // 定义person对象数组
+  let persons = reactive<Persons>([
+    { id: 'asdasdas', name: '张三', age: 12 },
+    { id: 'cse1asda', name: '李四', age: 13 }
+  ])
+</script>
 <style></style>
---
+++ code/src/components/Person.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="person">
+    <ul>
+      <li v-for="person in list" :key="person.id">姓名：{{ person.name }} -- 年龄 ：{{ person.age }}</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts" setup name="Person">
+  import { defineProps } from 'vue'
+  // 接受父组件传来的数组，并保存
+  let persons = defineProps(['list'])
+</script>
